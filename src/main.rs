73de9c4966baf9fcfa9ff4234d21351aa06d9abd
Ville M. Vainio <ville.vainio@basware.com>
2015-05-17 17:38:35 +0300

    use unstable walk_tree

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,19 @@
+#![feature(fs_walk)]
+
 use std::io;
-use std::fs::{self, PathExt, DirEntry};
+use std::fs::{self, PathExt, DirEntry, walk_dir};
 use std::path::Path;
 use std::env;
 
-// one possible implementation of fs::walk_dir only visiting files
-fn visit_dirs(dir: &Path, cb: &mut FnMut(DirEntry)) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in try!(fs::read_dir(dir)) {
-            let entry = try!(entry);
-            if entry.path().is_dir() {
-                try!(visit_dirs(&entry.path(), cb));
-            } else {
-                cb(entry);
-            }
-        }
-    }
-    Ok(())
-}
-
-fn ParseEntry(ent: DirEntry) {
-
-}
-
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() > 1 {
- 
-        visit_dirs(args[1], ParseEntry );
-        println!("The first argument is {}", args[1]);
+    let args: Vec<String> = env::args().collect();
+    let p = Path::new(&args[1]);
+    let walker = walk_dir(p).unwrap();
+    for w in walker {
+    	let ent = w.unwrap();
+ 		println!("({:?})", ent.path() );
+    	//println!("Path: {}", path);
     }
+    1;
+
 }
